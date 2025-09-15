35ed1192d3946c7b74982c0390ca38bd65073b6d
Juan Manuel Restrepo <juanmanuelrq@gmail.com>
2025-09-15 15:29:58 -0500

    ajuste

--- main.py
+++ main.py
@@ -379,4 +379,6 @@ async def update_polygon(polygon_id: str, polygon: Polygon):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
